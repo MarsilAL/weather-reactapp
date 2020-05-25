158694f06edd49efca19a8bd33b7af7900cac893
MarsilAL <alashkar.marsil@gmail.com>
2020-05-25 15:51:32 +0200

    Finished the CSS design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="app">
       <main>
@@ -16,6 +30,18 @@ function App() {
            className="search-bar" 
            placeholder="search"
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Berlin, Germany</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+        <div className="tempo">
+          20° C
+        </div>
+        <div className="weather">
+          Sunny
+        </div>
         </div>
       </main>
     </div>
