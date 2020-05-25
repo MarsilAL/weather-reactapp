29084d9b9b0a5c1f85205f0d4a2d84e9dc616e6e
MarsilAL <alashkar.marsil@gmail.com>
2020-05-25 17:29:16 +0200

    Updates

---
+++ precache-manifest.f48eece5e2fbc78e943ef37fc0b3aba4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ae8d082926e766f4ac392272e98ebc",
+    "url": "/weather-reactapp/index.html"
+  },
+  {
+    "revision": "0e9c96db4e70ed7c0eb2",
+    "url": "/weather-reactapp/static/css/main.7e395a35.chunk.css"
+  },
+  {
+    "revision": "9d580f7896c226108d7f",
+    "url": "/weather-reactapp/static/js/2.231ad675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-reactapp/static/js/2.231ad675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9c96db4e70ed7c0eb2",
+    "url": "/weather-reactapp/static/js/main.b5e35885.chunk.js"
+  },
+  {
+    "revision": "32a3144fda9569418a14",
+    "url": "/weather-reactapp/static/js/runtime-main.71dd137e.js"
+  },
+  {
+    "revision": "27c9aa2ada4362dba76e5b078ceaf27d",
+    "url": "/weather-reactapp/static/media/cold.27c9aa2a.jpg"
+  },
+  {
+    "revision": "ec62b2e2080945b88ea019883c166828",
+    "url": "/weather-reactapp/static/media/start.ec62b2e2.jpg"
+  },
+  {
+    "revision": "caf9cc09b876de556242518345841f6d",
+    "url": "/weather-reactapp/static/media/warm.caf9cc09.jpg"
+  }
+]);
